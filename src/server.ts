9ec6bc995228e2004e635e6e86cd9dae138d3464
Antoine Hakim <thenoobgrammer@gmail.com>
2023-07-07 18:55:44 -0400

    updates

--- src/server.ts
+++ src/server.ts
@@ -49,7 +49,6 @@ app
 function corsOptions() {
 	return {
 		methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
-		origin: 'http://localhost:3000',
 		optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 	}
 }
