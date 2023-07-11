7d798f39c881b743757a701b232568b1ae53d405
Antoine Hakim <thenoobgrammer@gmail.com>
2023-07-11 19:43:24 -0400

    * updates

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -5,6 +5,7 @@ import { validateAccessToken } from '../middleware'
 const router = express.Router()
 
 router.get('/', UserController.get)
+router.get('/create', UserController.create)
 router.put('/:id/settings', validateAccessToken, UserController.update)
 
 export default router
