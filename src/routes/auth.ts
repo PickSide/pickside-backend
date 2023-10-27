7c5d51bfa3a480f881594ff26598a0394fe67f5e
thenoobgrammer <thenoobgrammer@gmail.com>
2023-10-27 12:55:25 -0400

    * updates

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,7 +5,8 @@ const router = Router()
 
 router.get('/token', AuthController.getAccessToken)
 router.post('/login', AuthController.login)
-router.post('/googlelogin', AuthController.loginWithGoogle)
+router.post('/guest-login', AuthController.loginAsGuest)
+router.post('/google-login', AuthController.loginWithGoogle)
 router.post('/logout', AuthController.logout)
 
 export default router
