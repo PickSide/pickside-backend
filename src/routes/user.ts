a31a735129edc96646e2e631305a5275db87099a
thenoobgrammer <thenoobgrammer@gmail.com>
2024-01-02 19:41:31 -0500

    * no validation for me call

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -5,7 +5,7 @@ import { validateAccessToken } from '../middleware/session.middleware'
 const router = express.Router()
 
 router.get('/', UserController.getUsers)
-router.get('/me', validateAccessToken, UserController.getMe)
+router.get('/me', UserController.getMe)
 router.put('/deactivate/:userId', validateAccessToken, UserController.deactivate)
 router.get('/reactivate/:userId', validateAccessToken, UserController.reactivate)
 router.post('/create', UserController.create)
